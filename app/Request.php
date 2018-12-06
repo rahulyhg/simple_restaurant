eb1135866770736b9abc027a3a8f19f4833192c4
João Guilherme <joao.sczip@hotmail.com>
2018-12-06 15:35:13 -0200

    Model for requests

--- app/Request.php
+++ app/Request.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
-    //
+    protected $fillable = [
+        'client_id', 'table', 'total_price',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
